54f711a8cc7b3bdd2c32919801f42b9ec8fcde91
DevOPS Class <gcpcloudreddy@gmail.com>
2023-03-29 19:51:37 +0530

    changing msg

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Python Life DevOPS Class Today!";
+        return "Greetings from Python Life DevOPS Class Today - March 29 2023 !";
     }
 
 }
